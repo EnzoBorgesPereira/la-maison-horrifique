ce21b65b8b148dacad7fd86a4c8199fdfaea69f6
Enzo Borges Pereira <enzo077@outlook.fr>
2025-07-03 13:06:31 +0200

    fix(home): update imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
 import { Outlet } from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
 function App() {
   return (
     <div className="flex min-h-screen flex-col w-full dark:bg-gray-900 transition-colors duration-300">
       <Navbar />
-      <Footer />
+      <HomePage />
       <Outlet />
+      <Footer />
     </div>
   );
 }
