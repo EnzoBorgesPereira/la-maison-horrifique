a4c5a5f98f21646e60519f0b8121c92a9e5fe942
Enzo Borges Pereira <enzo077@outlook.fr>
2025-07-04 11:06:45 +0200

    chore: favicon

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({mode}) => ({
+  base: mode === 'development' ? "/la-maison-horrifique/" : undefined,
   plugins: [
     react(),
-    tailwindcss(),
+    tailwindcss()
   ],
-})
+}));
